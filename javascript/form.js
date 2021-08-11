4e84904fd9042d85cf9f5790187b930836c555bd
sebastian belettieri <sebastianbelettieri1@gmail.com>
2021-08-11 16:12:46 -0300

    finalizacion del formulario de contacto

---
+++ javascript/form.js
@@ -0,0 +1,23 @@
+
+$("#contForm").submit(async function (e) { 
+    e.preventDefault();
+
+    const form = new FormData(this);
+
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers:{
+            'Accept': 'aplication/json'
+        }
+    })
+    
+    if(response.ok){
+        this.reset();
+        console.log("se envio")
+
+        $("#msgEnviado").fadeIn("slow").fadeOut(5000);
+
+    }
+    
+});
